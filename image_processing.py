4c996f17a53aed8eb3d8149bebe4b527c47113f1
brownrat <101485105+brownrat@users.noreply.github.com>
2025-10-13 17:56:32 -0400

    Update image_processing.py
    fixed most of the problems with this

--- image_processing.py
+++ image_processing.py
@@ -2,26 +2,24 @@
 10/10/2025 -- bringing the 24 May 2024 version back as a separate file so OpenCV doesn't have to share
 space with matplotlib.
 
-I abandoned the approach of finding a "dominant wavelength" because I couldn't find a satisfactory way
-to do that. Human color appearance is not derived from LMS in the way CVS seems to assume.
-Instead I decided to skip that step and directly transform the source chromaticity into the target
-chromaticity by fitting the source sensitivity curves to the target, somewhat similar to a method
-available in micaToolbox (https://www.empiricalimaging.com/knowledge-base/spectral-sensitivity-based-cone-catch-models/).
+We skip the step of finding a dominant wavelength and directly transform the source chromaticity into
+the target chromaticity, as described here:
 
-The RGB values are scaled to the desired intensity using HSV. This is obviously not as good as
-converting to a color space designed to be more perceptually uniform like Lab or the LCH spaces,
-but any kind of conversion between sRGB and another color space seems to distort the hue and/or
-saturation.
+* https://www.empiricalimaging.com/knowledge-base/spectral-sensitivity-based-cone-catch-models/
+* https://doi.org/10.1111/2041-210X.12439
 
-micaToolbox uses not just a linear sum of the RGB channels but also up to 3-way interactions between
-them. I tried including 2-way interactions. The fitted curves look great on a plot, but the image
-looks like garbage. Using the monochromatic colors from optimal.py as a color chart, it seems the fit is
-only valid for monochromatic or fully saturated colors whereas anything closer to white is increasingly
-wrong. It's also strongly affected by the scale of both the input curves and input RGB values because
-they're multiplied. I get the closest to reasonable results by converting the curves and input to
-chromaticity and correcting the white balance of the output, but the original paper (https://doi.org/10.1111/2041-210X.12439)
-doesn't mention doing either of these. I also find the results are less likely to turn bright magenta
-and such if the lower bound is set to 0.
+Interactions can be set to either one-way (none; default) or two-way. I couldn't get two-way fits to
+produce good results for anything other than pure hues like the monochromatic wavelengths we fit
+the curves on, so like CVS we separate hue from saturation before converting the color. The final
+saturation is altered if the smallest output rgb value is not equal to 0 (>0 = less, <0 = more). I
+also convert the curves to chromaticity before fitting them because two-way interactions won't come
+out right otherwise. This avoids the problem CVS seems to have with dichromatic simulations where
+setting the L/M cone close to the human L cone removes the green channel and turns blue-green areas
+pure blue (earlier versions of my code also did this, as well as the reverse where setting it close
+to or below the M cone removes the red channel).
+
+Luminance is held constant by comparing the original XYZ Y value with the final value. This can
+possibly be changed to be relative to the target's luminosity function.
 """
 
 import math
@@ -52,18 +50,11 @@ imagename = args.image
 # but shifting the cones produces the opposite of the expected result.
 img = cv2.imread(imagename)
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-# white balance
-gray = [1/3,1/3,1/3,1/9,1/9,1/9]
-gray_r1 = sum(gray * c.s2t_r1)
-gray_r2 = sum(gray * c.s2t_r2)
-gray_r3 = sum(gray * c.s2t_r3)
 
 # transform hues for each pixel
 for x in range(img.shape[0]):
 	for y in range(img.shape[1]):
-		# report current pixel to determine whether there's an infinite loop
+		# report current pixel (for testing)
 		#print("pixel: " + str(x) + ", " + str(y))
 
 		# get pixel
@@ -73,14 +64,24 @@ for x in range(img.shape[0]):
 		rgb_r = pixel[0] / 255
 		rgb_g = pixel[1] / 255
 		rgb_b = pixel[2] / 255
-		#print(rgb_r)
-		#print(type(rgb_r))
 		
 		# undo gamma
 		rgb_r = c.linearize(rgb_r)
 		rgb_g = c.linearize(rgb_g)
 		rgb_b = c.linearize(rgb_b)
-		#print(type(rgb_r))
+		
+		# luminance
+		lum_in = rgb_r*0.2126729 + rgb_g*0.7151522 + rgb_b*0.0721750
+		
+		# saturate
+		max_rgb = max(rgb_r,rgb_g,rgb_b)
+		min_rgb = min(rgb_r,rgb_g,rgb_b)
+		if (max_rgb > 0): sat_in = min_rgb / max_rgb
+		else: sat_in = 0
+		rgb_r -= min_rgb
+		rgb_g -= min_rgb
+		rgb_b -= min_rgb
+		max_rgb_diff = max(rgb_r,rgb_g,rgb_b)
 		
 		# chromaticity
 		total = rgb_r + rgb_g + rgb_b
@@ -88,39 +89,12 @@ for x in range(img.shape[0]):
 			rgb_r /= total
 			rgb_g /= total
 			rgb_b /= total
-		#print(type(rgb_r))
 		
 		# combine
 		rgb_terms = [rgb_r, rgb_g, rgb_b, rgb_r*rgb_g, rgb_r*rgb_b, rgb_g*rgb_b]
-		##print(rgb_terms)
-		#print(rgb_terms * c.s2t_r1)
-		#print(rgb_terms * c.s2t_r2)
-		#print(rgb_terms * c.s2t_r3)
 		rgb_r1 = sum(rgb_terms * c.s2t_r1)
 		rgb_r2 = sum(rgb_terms * c.s2t_r2)
 		rgb_r3 = sum(rgb_terms * c.s2t_r3)
-		#print((rgb_r1,rgb_r2,rgb_r3))
-		#print(type(rgb_r1))
-		
-		if (args.whitebalance == "on"):
-			# white balance
-			if (gray_r1 > 0): rgb_r1 /= gray_r1
-			if (gray_r2 > 0): rgb_r2 /= gray_r2
-			if (gray_r3 > 0): rgb_r3 /= gray_r3
-		
-		# cut off
-		#rgb_r1 = min(rgb_r1,1)
-		#rgb_r2 = min(rgb_r2,1)
-		#rgb_r3 = min(rgb_r3,1)
-		rgb_r1 = max(rgb_r1,0)
-		rgb_r2 = max(rgb_r2,0)
-		rgb_r3 = max(rgb_r3,0)
-		
-		# redo gamma
-		rgb_r1 = c.gamma(rgb_r1)
-		rgb_r2 = c.gamma(rgb_r2)
-		rgb_r3 = c.gamma(rgb_r3)
-		#print(type(rgb_r1))
 		
 		# remove junk channels for lower dimensions
 		if (c.dimension == 2):
@@ -130,15 +104,47 @@ for x in range(img.shape[0]):
 		if (c.dimension == 1):
 			rgb_r2 = rgb_r1
 			rgb_r3 = rgb_r1
-
-		# HSV lightness
-		c_max = max(rgb_r1,rgb_r2,rgb_r3)
 		
-		if (c_max > 0):
-			img_rgb[x][y][0] = rgb_r1*img_hsv[x][y][2]/c_max
-			img_rgb[x][y][1] = rgb_r2*img_hsv[x][y][2]/c_max
-			img_rgb[x][y][2] = rgb_r3*img_hsv[x][y][2]/c_max
-		else: img_rgb[x][y] = [0,0,0]
+		# desaturate
+		max_rgb = max(rgb_r1,rgb_r2,rgb_r3)
+		if (max_rgb > 0):
+			rgb_r1 *= max_rgb_diff / max_rgb
+			rgb_r2 *= max_rgb_diff / max_rgb
+			rgb_r3 *= max_rgb_diff / max_rgb
+		rgb_r1 += min_rgb
+		rgb_r2 += min_rgb
+		rgb_r3 += min_rgb
+		
+		# cut off
+		# This is done after the previous step because negative numbers correspond to
+		# increased saturation.
+		rgb_r1 = max(rgb_r1,0)
+		rgb_r2 = max(rgb_r2,0)
+		rgb_r3 = max(rgb_r3,0)
+		
+		# output luminance
+		if (max_rgb > 0):
+			lum_out = rgb_r1*0.2126729 + rgb_r2*0.7151522 + rgb_r3*0.0721750
+			lum_diff = lum_in / lum_out
+			rgb_r1 *= lum_diff
+			rgb_r2 *= lum_diff
+			rgb_r3 *= lum_diff
+		
+		# redo gamma
+		rgb_r1 = c.gamma(rgb_r1)
+		rgb_r2 = c.gamma(rgb_r2)
+		rgb_r3 = c.gamma(rgb_r3)
+
+		# XYZ luminosity
+		# source: http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+		# The values have to be both clipped and rounded off or white parts of the image
+		# get mangled.
+		img_rgb[x][y][0] = min(255, round(rgb_r1 * 255))
+		img_rgb[x][y][1] = min(255, round(rgb_r2 * 255))
+		img_rgb[x][y][2] = min(255, round(rgb_r3 * 255))
+		if (canary):
+			print((rgb_r1*255,rgb_r2*255,rgb_r3*255))
+			print(img_rgb[x][y])
 
 # convert back to BGR
 img_result = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
